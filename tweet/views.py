dfcedf7e93f3240cf1b01ad08bf55de78e294e00
moonlight <moonlight@moonlightnoMacBook-Air.local>
2020-09-27 13:15:39 +0900

    webaoo no.4 formの手前 :wq

--- tweet/views.py
+++ tweet/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Post
 
 
@@ -8,6 +8,13 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['content']
+    template_name = 'tweet/post_create.html'
+    success_url = '/'
 
 
 
